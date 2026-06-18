0c9d09954c36b90a92514949fd89a2a47d2fdf39
joaoibira-ux <joao.ibira@gmail.com>
2026-06-18 20:48:39 -0300

    v1.15: ajusta posicao do fundo mobile para center 70%

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const VERSION = "ibira-v18";
+﻿const VERSION = "ibira-v19";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -9,7 +9,7 @@ const ASSETS = [
   "./caixa.html",
   "./areceber.html",
   "./apagar.html",
-  "./style.css?v=22",
+  "./style.css?v=23",
   "./app.js?v=3",
   "./clientes.js?v=2",
   "./estoque.js?v=2",
