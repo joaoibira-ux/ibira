eea5f9ad5e3f1ca7953447f5dc0cf01a09a19696
joaoibira-ux <joao.ibira@gmail.com>
2026-06-18 18:42:09 -0300

    v1.14: remove barra branca inferior com background-color escuro no mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const VERSION = "ibira-v17";
+﻿const VERSION = "ibira-v18";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -9,7 +9,7 @@ const ASSETS = [
   "./caixa.html",
   "./areceber.html",
   "./apagar.html",
-  "./style.css?v=21",
+  "./style.css?v=22",
   "./app.js?v=3",
   "./clientes.js?v=2",
   "./estoque.js?v=2",
