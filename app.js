1b89184b1814cd81e090090ee0988204184c0ac4
joaoibira-ux <joao.ibira@gmail.com>
2026-07-13 02:36:14 -0300

    Remover restricao de largura no desktop, voltar a usar a tela toda

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSAO_IBIRA = "1.44";
+const VERSAO_IBIRA = "1.45";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9zO5MO-lVAr6gea4t1pUuG-sC-s7stks",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const VERSION = "ibira-v48";
+﻿const VERSION = "ibira-v49";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -9,7 +9,7 @@ const ASSETS = [
   "./caixa.html",
   "./areceber.html",
   "./apagar.html",
-  "./style.css?v=29",
+  "./style.css?v=30",
   "./app.js?v=3",
   "./clientes.js?v=8",
   "./estoque.js?v=2",
