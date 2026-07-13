5953af2087afaebfc7b1868ce07865270376d159
joaoibira-ux <joao.ibira@gmail.com>
2026-07-13 02:39:52 -0300

    Aumentar altura do cabecalho e dos cards do menu no desktop

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const VERSION = "ibira-v49";
+﻿const VERSION = "ibira-v50";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -9,7 +9,7 @@ const ASSETS = [
   "./caixa.html",
   "./areceber.html",
   "./apagar.html",
-  "./style.css?v=30",
+  "./style.css?v=31",
   "./app.js?v=3",
   "./clientes.js?v=8",
   "./estoque.js?v=2",
