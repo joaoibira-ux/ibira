39e8e2f705688c20a8c2d9a98077c6e3bbd3fca0
joaoibira-ux <joao.ibira@gmail.com>
2026-07-20 02:00:54 -0300

    Menu do PC agora usa grid de cards igual ao do iPhone, ocupando a tela
    Antes os cards do menu apareciam como uma lista de linhas largas no
    desktop. Agora usam o mesmo layout vertical (icone em cima) das telas
    menores, em varias colunas que se ajustam para preencher o espaco
    disponivel.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const VERSION = "ibira-v62";
+﻿const VERSION = "ibira-v63";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -10,7 +10,7 @@ const ASSETS = [
   "./caixa.html",
   "./areceber.html",
   "./apagar.html",
-  "./style.css?v=25",
+  "./style.css?v=26",
   "./app.js?v=3",
   "./clientes.js?v=8",
   "./estoque.js?v=6",
