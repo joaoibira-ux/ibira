527199359c07c62b2501920bcd3e8ce9c5f6023d
joaoibira-ux <joao.ibira@gmail.com>
2026-07-20 02:04:31 -0300

    Aumenta bastante os cards do menu no desktop
    Icone, titulo e cards ficam bem maiores no PC (repeat auto-fill com
    minmax 340px, padding e gap maiores), mantendo o mobile inalterado.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSAO_IBIRA = "1.59";
+const VERSAO_IBIRA = "1.60";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9zO5MO-lVAr6gea4t1pUuG-sC-s7stks",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const VERSION = "ibira-v63";
+﻿const VERSION = "ibira-v64";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -10,7 +10,7 @@ const ASSETS = [
   "./caixa.html",
   "./areceber.html",
   "./apagar.html",
-  "./style.css?v=26",
+  "./style.css?v=27",
   "./app.js?v=3",
   "./clientes.js?v=8",
   "./estoque.js?v=6",
